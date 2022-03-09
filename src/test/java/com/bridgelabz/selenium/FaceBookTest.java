ae83f6ea88b7b574ae71e7c39b33d04437a9fa40
unknown <altamashbagwan1@gmail.com>
2022-03-09 23:58:55 +0530

    Refactored: Some changes in code and add gender selection related code

--- src/test/java/com/bridgelabz/selenium/FaceBookTest.java
+++ src/test/java/com/bridgelabz/selenium/FaceBookTest.java
@@ -2,8 +2,11 @@ package com.bridgelabz.selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 
 public class FaceBookTest {
@@ -18,14 +21,28 @@ public class FaceBookTest {
         driver.findElement(By.linkText("Create New Account")).click();
         Thread.sleep(2000);
         driver.findElement(By.name("firstname")).sendKeys("Altamash");
+        Thread.sleep(2000);
         driver.findElement(By.name("lastname")).sendKeys("Bagwan");
+        Thread.sleep(2000);
         driver.findElement(By.name("reg_email__")).sendKeys("altamashbagwan1@gmail.com");
+        Thread.sleep(2000);
         driver.findElement(By.name("reg_email_confirmation__")).sendKeys("altamashbagwan1@gmail.com");
+        Thread.sleep(2000);
         driver.findElement(By.name("reg_passwd__")).sendKeys("Altamash@77");
+        Thread.sleep(2000);
         driver.findElement(By.name("birthday_day")).sendKeys("26");
+        Thread.sleep(2000);
         driver.findElement(By.name("birthday_month")).sendKeys("April");
+        Thread.sleep(2000);
         driver.findElement(By.name("birthday_year")).sendKeys("1998");
-        driver.findElement(By.xpath("//*[contains(text(),'Male')]")).click();
+        Thread.sleep(2000);
+//        1st way
+//        driver.findElement(By.xpath("//*[contains(text(),'Male')]")).click();
+//        2nd way
+//        List<WebElement> gender = driver.findElements(By.name("sex"));
+//        gender.get(1).click();
+//        3rd way
+        driver.findElements(By.name("sex")).get(0).click();
         Thread.sleep(5000);
         driver.close();
 
