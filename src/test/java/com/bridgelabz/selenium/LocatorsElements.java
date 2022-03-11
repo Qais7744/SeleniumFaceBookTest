36ecd91252169a1a075d357257959ece8a0d81ec
unknown <altamashbagwan1@gmail.com>
2022-03-11 07:43:45 +0530

    Added: Identification of WebElements using LinkTest Locators

---
+++ src/test/java/com/bridgelabz/selenium/LocatorsElements.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LocatorsElements {
+    @Test
+    public void linkTest() throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Altamash\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        String baseUrl = "http://demo.guru99.com/test/link.html";
+        driver.get(baseUrl);
+        driver.findElement(By.linkText("click here")).click();
+        System.out.println("title of page is: " + driver.getTitle());
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
+}
